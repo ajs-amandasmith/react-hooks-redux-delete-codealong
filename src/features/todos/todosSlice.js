7af24c482b332e1d48e9f529aa0da1b592cf3d63
Amanda Smith <ajs.amandasmith@gmail.com>
2022-11-20 20:56:57 -0600

    commit tree 5a7486079a3d2feb835e92143dd5700807d2b57f

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react/cjs/react.production.min";
 
 const todosSlice = createSlice({
   name: "todos",
@@ -7,11 +9,18 @@ const todosSlice = createSlice({
   },
   reducers: {
     todoAdded(state, action) {
-      state.entities.push(action.payload);
+      state.entities.push({
+        id: uuid(), // use the uuid function to generate a unique id
+        text: action.payload
+      });
     },
+    todoRemoved(state, action) {
+      const index = state.entities.findIndex((todo) => todo.id === action.payload);
+      state.entities.splice(index, 1);
+    }
   },
 });
 
-export const { todoAdded } = todosSlice.actions;
+export const { todoAdded, todoRemoved } = todosSlice.actions;
 
 export default todosSlice.reducer;
